2180a833c4e0a6952399c418a85a2489a895f60c
Roshan Sawant <roshansawant70@yahoo.com>
2018-06-10 23:33:02 +0530

    added snake game

--- facebook_bday.py
+++ facebook_bday.py
@@ -1,8 +1,12 @@
 import facebook
+import access_token
 
-graph = facebook.GraphAPI('EAACEdEose0cBAC2ZAydYWjFhV5wfiDXSYkqZAn5A2n8daC8O5mfkVElFCkVF0hHn6ZBmon2jVDwVLXTZCjccZAU6GflbZBxCZC61d4ZAtATDMoJa35AUPIXp3KDlWAYqLj2NY1lsaau7erCWseLsJ71ALBLQrQWjXJ3ZAKEeWYvBR5vc4pTc57DioxitnxYZCq1gZAsR1KrHPPzkAZDZD')
+graph = facebook.GraphAPI('EAAaheEkH0A0BAMkZAdCU4DuWgnQZCCwqzly0lMJa1mFznTsMwI18ttV8expVglyh8H23ehNDkZAO9ecgdXoqZBvgZBVZCdve6FlFkMUpqZA1DpK9ZBMA7EI37WxwJYCYoOAxFyhCKT20DWBDi4gsRwAjDygqjsX6qOU69poMimUdWgZDZD')
+
 friends = graph.get_object("me/friends")
 print friends
+'''
 for friend in friends['data']:
     #print "{0} has id {1}".format(friend['name'].encode('utf-8'), friend['id'])
     print friend['name']
+'''
